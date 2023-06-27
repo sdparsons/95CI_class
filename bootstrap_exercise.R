9f7233d19156ace6e036fff5a0ec8ae7b1dd8234
sdparsons <sam.parsons@psy.ox.ac.uk>
2023-06-27 12:16:28 +0200

    update exercise

--- bootstrap_exercise.R
+++ bootstrap_exercise.R
@@ -30,3 +30,19 @@ mean(sample_BMIs)
 
 # extract 95% confidence interval using the 2.5 and 97.5 percentiles
 quantile(sample_BMIs, c(.025, .975))
+
+
+# visualise the bootstrapped distribution
+
+library("ggplot2")
+library("dplyr")
+as.data.frame(sample_BMIs) %>%
+  ggplot(aes(x = sample_BMIs)) +
+  geom_histogram() +
+  labs(x = "sample BMI", y = "Frequency") +
+  geom_vline(xintercept = mean(sample_BMIs)) + # mean
+  geom_vline(xintercept = quantile(sample_BMIs, .025)) + # lower CI
+  geom_vline(xintercept = quantile(sample_BMIs, .975)) + # upper CI
+  geom_label(aes(x = mean(sample_BMIs), y = 30, label = "Mean")) +
+  geom_label(aes(x = quantile(sample_BMIs, .025), y = 30, label = "lower 95% CI")) +
+  geom_label(aes(x = quantile(sample_BMIs, .975), y = 30, label = "upper 95% CI"))
